e6a0ebbb0634746f7032aee754938d9134e7b725
ngodingbentar <dewaqintoro@gmail.com>
2023-12-18 01:55:08 +0700

    added: landing page

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshold) {
+        console.log('window', window)
+        console.log('window.scrollY', window.scrollY)
+        console.log('threshold', threshold)
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [threshold]);
+
+  return scrolled;
+}
